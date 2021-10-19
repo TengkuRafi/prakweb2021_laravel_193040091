d0c57b43ca0b25f9f98ce67c990a66305daef675
TengkuRafi <63381758+TengkuRafi@users.noreply.github.com>
2021-10-19 19:19:03 +0700

    Menyelesaikan Vidio 5 tentang model,collections & controller
    Menyelesaikan Vidio 5 tentang model,collections & controller

--- applications/coba-laravel/routes/web.php
+++ applications/coba-laravel/routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -30,64 +33,5 @@ Route::get('/about', function () {
 
 
 
-Route::get('/blog', function () {
-    $blog_posts =[
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Tengku Muhammad Rafi",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem sapiente ratione porro eaque, expedita quaerat, in architecto assumenda illo rerum fugiat.
-             Eos ipsam natus dolorem dolor odit doloribus porro delectus perferendis aliquid, iure possimus ad laudantium consequatur molestiae, minima praesentium? Esse adipisci eius accusantium ex facere autem placeat culpa,
-            temporibus nisi excepturi voluptatem labore aliquam iusto corrupti veritatis impedit vitae illo. Maxime, impedit temporibus dolores, aperiam accusantium magnam maiores voluptate tempora,
-            porro laborum voluptates sapiente consectetur earum eos quibusdam veniam!"
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "UYE",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, illo accusamus iusto unde rerum enim sit temporibus eaque iure veritatis debitis aliquam optio recusandae.
-            Dolorem sequi laboriosam molestias ex a, explicabo neque laudantium necessitatibus ad eius distinctio iusto debitis consequatur corrupti rerum accusamus amet? Labore,
-            soluta minima? Doloremque, maxime consequuntur."
-        ],
-    ];
-
-    return view ('posts', [
-        "title" => "Posts",
-        "posts" => $blog_posts
-    ]);
-});
-
-
-
-//halaman Single Post
-Route::get('posts/{slug}', function($slug) {
-    $blog_posts =[
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Tengku Muhammad Rafi",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem sapiente ratione porro eaque, expedita quaerat, in architecto assumenda illo rerum fugiat.
-             Eos ipsam natus dolorem dolor odit doloribus porro delectus perferendis aliquid, iure possimus ad laudantium consequatur molestiae, minima praesentium? Esse adipisci eius accusantium ex facere autem placeat culpa,
-            temporibus nisi excepturi voluptatem labore aliquam iusto corrupti veritatis impedit vitae illo. Maxime, impedit temporibus dolores, aperiam accusantium magnam maiores voluptate tempora,
-            porro laborum voluptates sapiente consectetur earum eos quibusdam veniam!"
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "UYE",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, illo accusamus iusto unde rerum enim sit temporibus eaque iure veritatis debitis aliquam optio recusandae.
-            Dolorem sequi laboriosam molestias ex a, explicabo neque laudantium necessitatibus ad eius distinctio iusto debitis consequatur corrupti rerum accusamus amet? Labore,
-            soluta minima? Doloremque, maxime consequuntur."
-        ],
-    ];
-    $new_post = [];
-    foreach($blog_posts as $post){
-        if($post["slug"] === $slug){
-            $new_post = $post;
-        }
-    }
-    return view ('post', [
-        "title" => "Single Post",
-        "post" => $new_post
-    ]);
-});
+Route::get('/blog',[PostController::class, 'index']);
+Route::get('posts/{slug}', [PostController::class, 'show']);
