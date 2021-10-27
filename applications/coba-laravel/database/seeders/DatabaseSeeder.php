c5221a018e3c8c642c7c4c8647a4927290eb008a
TengkuRafi <63381758+TengkuRafi@users.noreply.github.com>
2021-10-27 19:55:30 +0700

    Menyelesaikan Vidio tentang Factory & Faker
    Menyelesaikan Vidio tentang Factory & Faker

--- applications/coba-laravel/database/seeders/DatabaseSeeder.php
+++ applications/coba-laravel/database/seeders/DatabaseSeeder.php
@@ -16,21 +16,23 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        
 
-        User::create([
-            'name' => 'Tengku Rafi',
-            'email' => 'tengku745@gmail.com',
-            'password' => bcrypt('12345')
+        // User::create([
+        //     'name' => 'Tengku Rafi',
+        //     'email' => 'tengku745@gmail.com',
+        //     'password' => bcrypt('12345')
 
-        ]);
+        // ]);
 
-        User::create([
-            'name' => 'Yasep',
-            'email' => 'yasep@gmail.com',
-            'password' => bcrypt('12345')
+        // User::create([
+        //     'name' => 'Yasep',
+        //     'email' => 'yasep@gmail.com',
+        //     'password' => bcrypt('12345')
 
-        ]);
+        // ]);
+
+        User::factory(3)->create();
 
         Category::create([
             'name' => 'Web Programming',
@@ -41,46 +43,48 @@ class DatabaseSeeder extends Seeder
             'name' => 'Personal',
             'slug' => 'personal'
         ]);
+
+        Post::factory(20)->create();
        
-        Post::create([
-            'title' => 'Judul Pertama',
-            'slug' => 'judul-pertama',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos vel aperiam fugiat natus minima repellat odit tenetur, nam nihil aut explicabo, voluptatem cupiditate enim, quasi dolor deserunt facilis expedita commodi facere amet',
-            'body'=> 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos vel aperiam fugiat natus minima repellat odit tenetur, nam nihil aut explicabo, voluptatem cupiditate enim, quasi dolor deserunt facilis expedita commodi facere amet. Nam, eius ad sit dolore aspernatur odio atque pariatur dolorem culpa blanditiis et laborum, ducimus velit, porro sunt dolor perspiciatis. Consectetur id praesentium possimus dolorem doloremque culpa debitis consequatur quo animi, ut voluptatum aliquam numquam quis odit voluptatem optio adipisci minima quia eaque veritatis. Accusamus, nam officia quam eum dignissimos aut. Vero, magni, eos minus nisi voluptatum ipsam veniam deleniti minima velit, suscipit nulla cupiditate dolores incidunt enim.',
-            'category_id' => 1,
-            'user_id' => 1
+        // Post::create([
+        //     'title' => 'Judul Pertama',
+        //     'slug' => 'judul-pertama',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos vel aperiam fugiat natus minima repellat odit tenetur, nam nihil aut explicabo, voluptatem cupiditate enim, quasi dolor deserunt facilis expedita commodi facere amet',
+        //     'body'=> 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos vel aperiam fugiat natus minima repellat odit tenetur, nam nihil aut explicabo, voluptatem cupiditate enim, quasi dolor deserunt facilis expedita commodi facere amet. Nam, eius ad sit dolore aspernatur odio atque pariatur dolorem culpa blanditiis et laborum, ducimus velit, porro sunt dolor perspiciatis. Consectetur id praesentium possimus dolorem doloremque culpa debitis consequatur quo animi, ut voluptatum aliquam numquam quis odit voluptatem optio adipisci minima quia eaque veritatis. Accusamus, nam officia quam eum dignissimos aut. Vero, magni, eos minus nisi voluptatum ipsam veniam deleniti minima velit, suscipit nulla cupiditate dolores incidunt enim.',
+        //     'category_id' => 1,
+        //     'user_id' => 1
 
-        ]);
+        // ]);
        
-        Post::create([
-            'title' => 'Judul Kedua',
-            'slug' => 'judul-ke-dua',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos vel aperiam fugiat natus minima repellat odit tenetur, nam nihil aut explicabo, voluptatem cupiditate enim, quasi dolor deserunt facilis expedita commodi facere amet',
-            'body'=> 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos vel aperiam fugiat natus minima repellat odit tenetur, nam nihil aut explicabo, voluptatem cupiditate enim, quasi dolor deserunt facilis expedita commodi facere amet. Nam, eius ad sit dolore aspernatur odio atque pariatur dolorem culpa blanditiis et laborum, ducimus velit, porro sunt dolor perspiciatis. Consectetur id praesentium possimus dolorem doloremque culpa debitis consequatur quo animi, ut voluptatum aliquam numquam quis odit voluptatem optio adipisci minima quia eaque veritatis. Accusamus, nam officia quam eum dignissimos aut. Vero, magni, eos minus nisi voluptatum ipsam veniam deleniti minima velit, suscipit nulla cupiditate dolores incidunt enim.',
-            'category_id' => 1,
-            'user_id' => 1
+        // Post::create([
+        //     'title' => 'Judul Kedua',
+        //     'slug' => 'judul-ke-dua',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos vel aperiam fugiat natus minima repellat odit tenetur, nam nihil aut explicabo, voluptatem cupiditate enim, quasi dolor deserunt facilis expedita commodi facere amet',
+        //     'body'=> 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos vel aperiam fugiat natus minima repellat odit tenetur, nam nihil aut explicabo, voluptatem cupiditate enim, quasi dolor deserunt facilis expedita commodi facere amet. Nam, eius ad sit dolore aspernatur odio atque pariatur dolorem culpa blanditiis et laborum, ducimus velit, porro sunt dolor perspiciatis. Consectetur id praesentium possimus dolorem doloremque culpa debitis consequatur quo animi, ut voluptatum aliquam numquam quis odit voluptatem optio adipisci minima quia eaque veritatis. Accusamus, nam officia quam eum dignissimos aut. Vero, magni, eos minus nisi voluptatum ipsam veniam deleniti minima velit, suscipit nulla cupiditate dolores incidunt enim.',
+        //     'category_id' => 1,
+        //     'user_id' => 1
 
-        ]);
+        // ]);
 
-        Post::create([
-            'title' => 'Judul Ketiga',
-            'slug' => 'judul-ke-tiga',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos vel aperiam fugiat natus minima repellat odit tenetur, nam nihil aut explicabo, voluptatem cupiditate enim, quasi dolor deserunt facilis expedita commodi facere amet',
-            'body'=> 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos vel aperiam fugiat natus minima repellat odit tenetur, nam nihil aut explicabo, voluptatem cupiditate enim, quasi dolor deserunt facilis expedita commodi facere amet. Nam, eius ad sit dolore aspernatur odio atque pariatur dolorem culpa blanditiis et laborum, ducimus velit, porro sunt dolor perspiciatis. Consectetur id praesentium possimus dolorem doloremque culpa debitis consequatur quo animi, ut voluptatum aliquam numquam quis odit voluptatem optio adipisci minima quia eaque veritatis. Accusamus, nam officia quam eum dignissimos aut. Vero, magni, eos minus nisi voluptatum ipsam veniam deleniti minima velit, suscipit nulla cupiditate dolores incidunt enim.',
-            'category_id' => 2,
-            'user_id' => 1
+        // Post::create([
+        //     'title' => 'Judul Ketiga',
+        //     'slug' => 'judul-ke-tiga',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos vel aperiam fugiat natus minima repellat odit tenetur, nam nihil aut explicabo, voluptatem cupiditate enim, quasi dolor deserunt facilis expedita commodi facere amet',
+        //     'body'=> 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos vel aperiam fugiat natus minima repellat odit tenetur, nam nihil aut explicabo, voluptatem cupiditate enim, quasi dolor deserunt facilis expedita commodi facere amet. Nam, eius ad sit dolore aspernatur odio atque pariatur dolorem culpa blanditiis et laborum, ducimus velit, porro sunt dolor perspiciatis. Consectetur id praesentium possimus dolorem doloremque culpa debitis consequatur quo animi, ut voluptatum aliquam numquam quis odit voluptatem optio adipisci minima quia eaque veritatis. Accusamus, nam officia quam eum dignissimos aut. Vero, magni, eos minus nisi voluptatum ipsam veniam deleniti minima velit, suscipit nulla cupiditate dolores incidunt enim.',
+        //     'category_id' => 2,
+        //     'user_id' => 1
 
-        ]);
+        // ]);
 
-        Post::create([
-            'title' => 'Judul Empat',
-            'slug' => 'judul-ke-empat',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos vel aperiam fugiat natus minima repellat odit tenetur, nam nihil aut explicabo, voluptatem cupiditate enim, quasi dolor deserunt facilis expedita commodi facere amet',
-            'body'=> 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos vel aperiam fugiat natus minima repellat odit tenetur, nam nihil aut explicabo, voluptatem cupiditate enim, quasi dolor deserunt facilis expedita commodi facere amet. Nam, eius ad sit dolore aspernatur odio atque pariatur dolorem culpa blanditiis et laborum, ducimus velit, porro sunt dolor perspiciatis. Consectetur id praesentium possimus dolorem doloremque culpa debitis consequatur quo animi, ut voluptatum aliquam numquam quis odit voluptatem optio adipisci minima quia eaque veritatis. Accusamus, nam officia quam eum dignissimos aut. Vero, magni, eos minus nisi voluptatum ipsam veniam deleniti minima velit, suscipit nulla cupiditate dolores incidunt enim.',
-            'category_id' => 2,
-            'user_id' => 2
+        // Post::create([
+        //     'title' => 'Judul Empat',
+        //     'slug' => 'judul-ke-empat',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos vel aperiam fugiat natus minima repellat odit tenetur, nam nihil aut explicabo, voluptatem cupiditate enim, quasi dolor deserunt facilis expedita commodi facere amet',
+        //     'body'=> 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos vel aperiam fugiat natus minima repellat odit tenetur, nam nihil aut explicabo, voluptatem cupiditate enim, quasi dolor deserunt facilis expedita commodi facere amet. Nam, eius ad sit dolore aspernatur odio atque pariatur dolorem culpa blanditiis et laborum, ducimus velit, porro sunt dolor perspiciatis. Consectetur id praesentium possimus dolorem doloremque culpa debitis consequatur quo animi, ut voluptatum aliquam numquam quis odit voluptatem optio adipisci minima quia eaque veritatis. Accusamus, nam officia quam eum dignissimos aut. Vero, magni, eos minus nisi voluptatum ipsam veniam deleniti minima velit, suscipit nulla cupiditate dolores incidunt enim.',
+        //     'category_id' => 2,
+        //     'user_id' => 2
 
-        ]);
+        // ]);
 
     }
 }
