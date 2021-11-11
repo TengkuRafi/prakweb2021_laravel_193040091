1ec3c30a83dc4d2174df0a3f61cc82f3d45ea533
TengkuRafi <63381758+TengkuRafi@users.noreply.github.com>
2021-11-11 22:05:36 +0700

    Menyelesaikan vidio 13 dan 14
    Menyelesaikan vidio 13 dan 14

--- applications/coba-laravel/resources/views/layouts/main.blade.php
+++ applications/coba-laravel/resources/views/layouts/main.blade.php
@@ -6,7 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" 
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
     <title>WPU Blog | {{ $title }}</title>
   </head>
@@ -18,8 +19,14 @@
         @yield('container')
     </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" 
+    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
-   
+    {{-- Bootsrap Icons --}}
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.0/font/bootstrap-icons.css">
+
+   {{-- My Style --}}
+
+   <link rel="stylesheet" href="/css/style.css">
   </body>
 </html>
--- applications/coba-laravel/routes/web.php
+++ applications/coba-laravel/routes/web.php
@@ -1,11 +1,13 @@
 <?php
 
 use App\Models\Post;
+use App\Models\User;
+use App\Models\Category;
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
-
-use App\Models\Category;
-use App\Models\User;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 
 
 /*
@@ -21,13 +23,16 @@ use App\Models\User;
 
 Route::get('/', function () {
     return view ('home',[
-        "title" => "Home"
+        "title" => "Home",
+        "active" => 'home'
+
     ]);
 });
 
 Route::get('/about', function () {
     return view ('about', [
         "title" => "About",
+        "active" => 'about',
         "name" => "Tengku Muhammad Rafi",
         "email" => "tengku745@gmail.com",
         "image" => "gampah.jpg"
@@ -55,9 +60,7 @@ Route::get('/categories/{category:slug}', function(Category $category){
     ]);
 });
 
-Route::get('/authors/{author:username}', function(User $author){
-    return view('posts',[
-        'title' => "Post By Author : $author->name",
-        'posts' => $author->posts->load('category', 'author'),
-    ]);
-});
+
+Route::get('/login' , [LoginController::class, 'index']);
+
+Route::get('/register' , [RegisterController::class, 'index']);
