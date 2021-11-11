0cf7f73790601b03e7765a085b99557e37cdf72b
TengkuRafi <63381758+TengkuRafi@users.noreply.github.com>
2021-11-12 01:06:18 +0700

    Menyelesaikan vidio 15 tentang User Login& middleware
    Menyelesaikan vidio 15 tentang User Login& middleware

---
+++ applications/coba-laravel/resources/views/dashboard/index.blade.php
@@ -0,0 +1,1 @@
+<h1>Welcome, Tengku</h1>
--- applications/coba-laravel/routes/web.php
+++ applications/coba-laravel/routes/web.php
@@ -61,7 +61,11 @@ Route::get('/categories/{category:slug}', function(Category $category){
 });
 
 
-Route::get('/login' , [LoginController::class, 'index']);
+Route::get('/login' , [LoginController::class, 'index'])->name('login')->middleware('guest');
+Route::post('/login' , [LoginController::class, 'authenticate']);
+Route::post('/logout' , [LoginController::class, 'logout']);
 
-Route::get('/register' , [RegisterController::class, 'index']);
+Route::get('/register' , [RegisterController::class, 'index'])->middleware('guest');
 Route::post('/register' , [RegisterController::class, 'store']);
+
+Route::get('/dashboard', 'App\Http\Controllers\DashboardController@index')->middleware('auth');
