9f4d983bf0f794fdb0a879d7c4bb94efab3be0de
TengkuRafi <63381758+TengkuRafi@users.noreply.github.com>
2021-11-25 12:07:45 +0700

    Mengerjakan vidio 17 tentang Dashboard UI
    Mengerjakan vidio 17 tentang Dashboard UI

--- applications/coba-laravel/routes/web.php
+++ applications/coba-laravel/routes/web.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\DashboardPostController;
 
 
 /*
@@ -68,4 +69,9 @@ Route::post('/logout' , [LoginController::class, 'logout']);
 Route::get('/register' , [RegisterController::class, 'index'])->middleware('guest');
 Route::post('/register' , [RegisterController::class, 'store']);
 
-Route::get('/dashboard', 'App\Http\Controllers\DashboardController@index')->middleware('auth');
+Route::get('/dashboard', function(){
+    return view('dashboard.index');
+})->middleware('auth');
+
+
+Route::resource('/dashboard/posts', DashboardPostController::class)->middleware('auth');
