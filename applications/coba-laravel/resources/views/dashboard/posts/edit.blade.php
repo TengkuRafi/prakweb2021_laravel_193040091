c402ad1edc4abe09c3cbeff4103d7ad3080785e2
TengkuRafi <63381758+TengkuRafi@users.noreply.github.com>
2021-12-01 12:17:48 +0700

    Menyelesaikan Vidio 22 Priview,Update & Delete Image
    Menyelesaikan Vidio 22 Priview,Update & Delete Image

--- applications/coba-laravel/resources/views/dashboard/posts/edit.blade.php
+++ applications/coba-laravel/resources/views/dashboard/posts/edit.blade.php
@@ -3,9 +3,9 @@
 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
     <h1 class="h2">Edit Post</h1>
   </div>
+
 <div class="col-lg-8">
-    
-    <form method="post" action="/dashboard/posts/{{ $post->slug }}" class="mb-5">
+    <form method="post" action="/dashboard/posts/{{ $post->slug }}" class="mb-5" enctype="multipart/form-data">
       @method('put')
         @csrf
         <div class="mb-3">
@@ -42,6 +42,22 @@
         </select>
       </div>
       
+      <div class="mb-3">
+        <label for="image" class="form-label">Post Image</label>
+        <input type="hidden" name="oldImage" value="{{ $post->image }}">
+        @if ($post->image)
+        <img src="{{ asset('storage/' . $post->image) }}" class="img-preview img-fluid mb-3 col-sm-5 d-block">
+        @else
+        <img class="img-preview img-fluid mb-3 col-sm-5">         
+        @endif    
+        <input class="form-control @error('image') is-invalid @enderror" type="file" id="image" name="image" onchange="previewImage()">
+        @error('image')
+        <div class="invalid-feedback"> 
+        {{ $message }}
+        </div>
+        @enderror 
+      </div>
+
       <div class="mb-3">
         <label for="body" class="form-label">Body</label>
         @error('body')
